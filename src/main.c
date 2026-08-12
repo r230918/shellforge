d749a506dca03687450773b2a0123313a223c95b
Shaik Abdulla <shaikabdulla2k6@gmail.com>
2026-08-12 11:34:11 +0000

    MileStone-2 added successfully

--- src/main.c
+++ src/main.c
@@ -1,56 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 #include "history.h"
+#include "token.h"
+#include "lexer.h"
+
 
 int main(void)
 {
-	// Display a welcome banner when the shell starts
-	printf("====================================\n");
-	printf("	Shellforge \n");
-	printf(" A Unix Style Shell written in C\n");
-	printf("====================================\n");
+    // Display a welcome banner when the shell starts
+    printf("=====================================\n");
+    printf("      Shellforge \n");
+    printf(" A Unix Style Shell written in C\n");
+    printf("=====================================\n");
 
- // Initializing History
-  using_history();
-  
-  char *line;
-	while(1)
-	{
-		line = readline("shellforge$ ");
+ token_list_t tokens;
+ using_history(); 
+ char *line;
 
-		if(line == NULL)
-		{
-			printf("\nGoodBye!\n");
-			break;
-		}
+    while (1)
+    {
+        line = readline("shellforge$ ");
 
-		if(strlen(line) == 0)
-		{
-			free(line);
-			continue;
-		}
+      if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            printf("Exiting...\n");
+            break;
+        }
 
-		if (strcmp(line, "history") == 0)
-		{
-		print_history();
-		free(line);
-		continue;
-		}
-		add_history(line);
-		printf(" YOU ENTERED : %s\n", line);
-		
-		if(strcmp(line, "exit") == 0)
-		{
-			free(line);
-			printf("Exiting...\n");
-			break;
-		}
-		free(line);
-	}
-	return 0;
+
+        if (line == NULL)
+        {
+            printf("\nGoodbye!\n");
+            break;
+        }
+
+        if (strlen(line) == 0)
+        {
+            free(line);
+            continue;
+        }
+
+        if (strcmp(line, "history") == 0)
+        {
+           print_history();
+           free(line);
+           continue;
+        }
+
+        add_history(line);
+  lexer(line, &tokens);
+        token_print(&tokens);
+     
+       free(line);
+    }    
+    return 0;
 }
-
-
