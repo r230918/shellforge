22119a3f9c941a951f40a590102ab97b9751f910
Shaik Abdulla <shaikabdulla2k6@gmail.com>
2026-08-19 08:58:13 +0000

    Milestone2 Parser and Expand files are added

--- src/main.c
+++ src/main.c
@@ -3,11 +3,10 @@
 #include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
-#include "history.h"
 #include "token.h"
 #include "lexer.h"
-
-
+#include "parser.h"
+#include "expand.h"
 int main(void)
 {
     // Display a welcome banner when the shell starts
@@ -16,46 +15,46 @@ int main(void)
     printf(" A Unix Style Shell written in C\n");
     printf("=====================================\n");
 
- token_list_t tokens;
- using_history(); 
+  token_list_t tokens;
+ pipeline_t pipeline;
+ 
  char *line;
 
     while (1)
     {
         line = readline("shellforge$ ");
-
-      if (strcmp(line, "exit") == 0)
-        {
-            free(line);
-            printf("Exiting...\n");
-            break;
-        }
-
-
         if (line == NULL)
         {
             printf("\nGoodbye!\n");
             break;
         }
-
         if (strlen(line) == 0)
         {
             free(line);
             continue;
         }
 
-        if (strcmp(line, "history") == 0)
-        {
-           print_history();
-           free(line);
-           continue;
-        }
-
-        add_history(line);
-  lexer(line, &tokens);
+         add_history(line);
+	lexer(line, &tokens);
         token_print(&tokens);
-     
-       free(line);
+        
+
+	if(parser(&tokens, &pipeline))
+	{
+		expand_variables(&pipeline);
+    		pipeline_print(&pipeline);
+	}
+        
+
+
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            printf("Exiting...\n");
+            break;
+        }
+	free(line);
     }    
     return 0;
 }
+
